81b9a9c8169ff5f4f34c540fa43d551a52edb43d
Carnotfr <71776621+Carnotfr@users.noreply.github.com>
2023-01-22 14:37:16 +0100

    Update Q3_Partition.py

--- Q3_Partition.py
+++ Q3_Partition.py
@@ -1,6 +1,3 @@
-#   Created by Elshad Karimov on 18/05/2020.
-#   Copyright © 2020 AppMillers. All rights reserved.
-
 #  Question 3 - Write code to partition a linked list around a value x, 
 #               such that all nodes less than x come before all nodes greater than or equal to x. 
 
